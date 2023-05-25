3ff9194919a8cdad1d6c7ce2551c36521b8e224b
PnthrLeo <leon-1402@mail.ru>
2023-05-25 23:53:27 +0300

    small fixes

--- decrypt_secrets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "$PGP_PRIVATE_KEY" > private.key || true
-echo "$PGP_PUBLIC_KEY" > public.key || true
-gpg --import public.key || true
-gpg --import private.key || true
-
-sops --decrypt -in-place database/config.json || true
-sops --decrypt --in-place tests/fixtures/db_config.json || true
--- encrypt_secrets.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sops --encrypt -in-place --pgp $PGP_PUBLIC_KEY database/config.json
-sops --encrypt --in-place --pgp $PGP_PUBLIC_KEY tests/fixtures/db_config.json
